0898cbcf1653abf454ac1f7326a27531c6f60561
Thomas Adam <thomas@xteddy.org>
2018-10-13 13:09:51 +0100

    Add back datatables section

--- Guardian-Cryptic-Crosswords/lib/Guardian/Cryptic/Plugins/chart5.pm
+++ Guardian-Cryptic-Crosswords/lib/Guardian/Cryptic/Plugins/chart5.pm
@@ -64,13 +64,35 @@ sub render
 
 	my $interdata = $self->interpolate();
 
+	my $interim_js = [];
+
+	foreach my $j (@$interdata) {
+		my $clues = join '<br />', @{$j->{'clues'}};
+		my $nums  = join '<br />', @{$j->{'cnumbers'}};
+		push @{$interim_js},
+			[
+				$j->{'id'}->{'name'},
+				$j->{'id'}->{'solution'},
+				$clues,
+				$nums,
+			]
+	};
+
 	my $data = {
 		'title' => "Number of duplicate answers and their questions",
 		'preamble' => "This table shows the number of times a given " .
 		              "clue has been used and the different questions " .
 			      "which have been used to make up that clue.",
 		'order' => 5,
-		'table' => $interdata,
+		'js' => {
+			data => $interim_js,
+			columns => [
+				{ 'title' => 'Setter' },
+				{ 'title' => 'Answer' },
+				{ 'title' => 'Clues'  },
+				{ 'title' => 'Crossword' },
+			],
+		}
 	};
 
 	$self->save(file => $tmpl_file, content => $data);
