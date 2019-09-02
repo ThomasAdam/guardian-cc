1f3fcd883f00bce4ed363ff23385e8af97cba697
Thomas Adam <thomas@xteddy.org>
2019-09-02 18:47:35 +0100

    Update scraper

--- tools/web-scraper.py
+++ tools/web-scraper.py
@@ -22,10 +22,10 @@ This uses a crude sequence of numbers to derive the URL for the cryptic.
 # Guardian website only has data from 1999.  "30000" is very optimistic and
 # should be future proof.
 cryptic_lower_id = 21620
-cryptic_upper_id = 39480
+cryptic_upper_id = 50000
 
-prize_lower_id = 21622
-prize_upper_id = 50000
+attempts = 0
+was_written = False
 
 # CWD for where to place downloaded files, relative to here.
 cwd = os.getcwd()
@@ -33,7 +33,6 @@ cwd = os.getcwd()
 # Tuple for range getting crosswords; this is recalculated if we fetched from
 # a known last id.
 CRYPTIC = (cryptic_lower_id, cryptic_upper_id, "cryptic")
-PRIZE = (prize_lower_id, prize_upper_id, "prize")
 
 # The last read entry is stored externally.  If present, use it to go from
 # there, to upper_id.
@@ -42,49 +41,32 @@ cryptic_lrid_file = cwd + "/tools/cryptic_last_read_id"
 if os.path.isfile(cryptic_lrid_file):
     with open(cryptic_lrid_file, "r") as clrfile:
         lower_id = clrfile.read().replace('\n', '')
+        last_id_fetched = lower_id
         print("Current cryptic id is: {}".format(lower_id))
 
         # The next crossword...
-        cryptic_lower_id = int(lower_id) + 1
-        CRYPTIC = (cryptic_lower_id, cryptic_upper_id, "cryptic")
         clrfile.close()
 
-prize_lrid_file = cwd + "/tools/prize_last_read_id"
-if os.path.isfile(prize_lrid_file):
-    with open(prize_lrid_file, "r") as clrfile:
-        lower_id = clrfile.read().replace('\n', '')
-        print("Current prize id is: {}".format(lower_id))
+def url_fetch(cctype, ccid):
+    url = "https://www.theguardian.com/crosswords/" + cctype + "/" + str(ccid)
+    result = requests.get(url)
+    global attempts
+    if result.status_code == 200:
+        attempts -= 1
 
-        # The next crossword...
-        prize_lower_id = int(lower_id) + 1
-        PRIZE = (prize_lower_id, prize_upper_id, "prize")
-        clrfile.close()
+    if result.status_code == 404:
+        attempts += 1
 
-# Set the last_id_fetched to the lowest -- as we iterate over the range of
-# IDs, this will get overwritten appropriately.
-cryptic_last_id_fetched = cryptic_lower_id - 1
-prize_last_id_fetched = prize_lower_id + 1
+    return result
 
-# Kick off!
-CROSSWORD_TYPE = [CRYPTIC, PRIZE]
-for crossword in CROSSWORD_TYPE:
-    crossword_type = crossword[2]
+def try_one(crossword_type, num):
+            was_written = False
+            print("{}/{}... ".format(crossword_type, num), end='')
+            result = url_fetch(crossword_type, num)
+            sc = result.status_code
+            if sc == 404:
+                    return -1
 
-    # If at first you don't succeed...
-    tries = 3
-    goes = 0
-
-    if crossword_type == "prize":
-        lrid_file = prize_lrid_file
-        last_id_fetched = prize_last_id_fetched + 2
-    else:
-        lrid_file = cryptic_lrid_file
-        last_id_fetched = cryptic_last_id_fetched
-
-    for num in xrange(*crossword[:2]):
-        try:
-            url = "https://www.theguardian.com/crosswords/" + crossword_type + "/" + str(num)
-            result = requests.get(url)
             clues = ""
             clues1 = ""
 
@@ -93,41 +75,64 @@ for crossword in CROSSWORD_TYPE:
             # The web site seemingly has two different classes for storing the
             # crossword information -- try both, if they fail, we're doomed
             # anyway.
-            try:
-                clues1 = soup.find("div", {"class":"js-crossword "})
+            clues1 = soup.find("div", {"class":"js-crossword"})
+            if clues1 != None:
                 clues = clues1["data-crossword-data"]
-            except:
+            else:
                 clues1 = soup.find("div", {"class":"js-crossword has-grouped-clues"})
-                clues = clues1["data-crossword-data"]
+                if clues1 != None:
+                    clues = clues1["data-crossword-data"]
 
             # Serialise the JSON
-            clues_json = json.loads(clues)
+            try:
+                clues_json = json.loads(clues)
+            except ValueError:
+                print("Invalid JSON... ({})".format(clues))
+                return -1
+
+            if not "creator" in clues_json:
+                print("Skipping {} as no creator key".format(num))
+                with open("/tmp/{}.json".format(num), "wb") as file:
+                    json.dump(clues_json, file, indent = 4)
+                return -1
 
             # Create the directory if necessary!
-            if not os.path.exists(cwd + "/crosswords/" + crossword_type + "/setter/" + \
-                    clues_json["creator"]["name"]):
-                os.makedirs(cwd + "/crosswords/" + crossword_type + "/setter/" + \
-                        clues_json["creator"]["name"]);
+            if not os.path.exists(cwd + "/crosswords/" + crossword_type \
+                    + "/setter/" + clues_json["creator"]["name"]):
+                        os.makedirs(cwd + "/crosswords/" + crossword_type + \
+                                    "/setter/" + clues_json["creator"]["name"])
 
             save_name = cwd + "/crosswords/" + crossword_type + "/setter/" + \
                 clues_json["creator"]["name"] + "/" + str(num) + ".JSON"
+            save_name = save_name.encode('utf-8')
 
-            with open(save_name, "wb") as file:
-                json.dump(clues_json, file, indent = 4)
+            if os.path.exists(save_name):
+                print("Skipping {} as it's already saved ({})".format(num, save_name))
+            else:
+                with open(save_name, "wb") as file:
+                    json.dump(clues_json, file, indent = 4)
 
-            # Reset the counter; we want three consecutive misses to be a failure.
-            goes -= 1
-            if goes < 0:
-                goes = 0
+                print("Written [{}]: {}/{} to disk...".format(crossword_type, clues_json["creator"]["name"], num))
+                last_id_fetched = num
+                was_written = True
 
-            print("Written [{}]: {}/{} to disk...".format(crossword_type, clues_json["creator"]["name"], num))
+            return num
+        
+# Kick off!
+lrid_file = cryptic_lrid_file
+# If at first you don't succeed...
+tries = 3
+
+for num in range(int(last_id_fetched), cryptic_upper_id):
+        sys.stdout.flush()
+        ret = try_one("cryptic", num)
+        ret = try_one("prize", num)
+        sys.stdout.flush()
+        if was_written:
             last_id_fetched = num
             os.system("./tools/import.pl {}".format(save_name));
-        except:
-            goes += 1
-            print("Couldn't find crossword: [{}]: {}".format(crossword_type, num))
-            if goes >= tries:
-                with open(lrid_file, "wb") as lrfile:
-                    lrfile.write(str(last_id_fetched))
-                    lrfile.close()
-                break;
+        if attempts >= tries:
+            with open(lrid_file, "wb") as lrfile:
+                lrfile.write(str(last_id_fetched))
+                lrfile.close()
+            break
