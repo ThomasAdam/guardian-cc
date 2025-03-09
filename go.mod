c7fd0256347ed5cc2912e6ae1ed53126d12c4fce
Thomas Adam <ThomasAdam@users.noreply.github.com>
2025-03-09 16:18:48 +0000

    SYNC

---
+++ go.mod
@@ -0,0 +1,34 @@
+module guardian-cc
+
+go 1.24.1
+
+require (
+	github.com/apache/arrow-go/v18 v18.1.0 // indirect
+	github.com/duckdb/duckdb-go-bindings v0.1.9 // indirect
+	github.com/duckdb/duckdb-go-bindings/darwin-amd64 v0.1.4 // indirect
+	github.com/duckdb/duckdb-go-bindings/darwin-arm64 v0.1.4 // indirect
+	github.com/duckdb/duckdb-go-bindings/linux-amd64 v0.1.4 // indirect
+	github.com/duckdb/duckdb-go-bindings/linux-arm64 v0.1.4 // indirect
+	github.com/duckdb/duckdb-go-bindings/windows-amd64 v0.1.4 // indirect
+	github.com/go-viper/mapstructure/v2 v2.2.1 // indirect
+	github.com/goccy/go-json v0.10.5 // indirect
+	github.com/google/flatbuffers v25.1.24+incompatible // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/gookit/color v1.5.4 // indirect
+	github.com/gookit/goutil v0.6.18 // indirect
+	github.com/klauspost/compress v1.17.11 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.9 // indirect
+	github.com/marcboeker/go-duckdb/arrowmapping v0.0.2 // indirect
+	github.com/marcboeker/go-duckdb/mapping v0.0.2 // indirect
+	github.com/marcboeker/go-duckdb/v2 v2.0.0 // indirect
+	github.com/pierrec/lz4/v4 v4.1.22 // indirect
+	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
+	github.com/zeebo/xxh3 v1.0.2 // indirect
+	golang.org/x/exp v0.0.0-20250128182459-e0ece0dbea4c // indirect
+	golang.org/x/mod v0.22.0 // indirect
+	golang.org/x/sync v0.10.0 // indirect
+	golang.org/x/sys v0.29.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+	golang.org/x/tools v0.29.0 // indirect
+	golang.org/x/xerrors v0.0.0-20240903120638-7835f813f4da // indirect
+)
---
+++ main.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/gookit/goutil/dump"
+
+	_ "github.com/marcboeker/go-duckdb/v2"
+)
+
+// FIXME: should be in its own object.
+func db_run_stmt_transactional(db *sql.DB, s string) {
+
+	tx, err := db.Begin()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = db.Exec(s)
+	if err != nil {
+		log.Fatalf("Couldn't run: %v", err)
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func db_create() *sql.DB {
+	log.Println("Creating database...")
+	db, err := sql.Open("duckdb", "cc.duck")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return db
+}
+
+func cc_get_setter_count(db *sql.DB) {
+	const ccSetterQuery string = `
+	copy(select ifnull(creator->>'name', 'unknown') as name,
+	crosswordType,
+	count (*) as num
+	from cc
+	group by all
+	order by crosswordType, num desc, name) to 'run1.json' (ARRAY)`
+
+	log.Print("Running setter count query...")
+	db_run_stmt_transactional(db, ccSetterQuery)
+	log.Print("    Done")
+
+	/*
+	   	type cc_count struct {
+	   		Name          string `json:"name"`
+	   		CrosswordType string `json:"crosswordType"`
+	   		Count         int64  `json:"num"`
+	   	}
+
+	   rows, err := db.Query(ccSetterQuery)
+
+	   	if err != nil {
+	   		log.Fatal("Couldn't run ccSetterQuery")
+	   	}
+
+	   defer rows.Close()
+
+	   var ccRes []cc_count
+
+	   // Loop through rows, using Scan to assign column data to struct fields.
+
+	   	for rows.Next() {
+	   		var ccc cc_count
+	   		if err := rows.Scan(&ccc.Name, &ccc.CrosswordType, &ccc.Count); err != nil {
+	   			log.Fatal("Couldn't scan rows: ", err.Error())
+	   		}
+	   		ccRes = append(ccRes, ccc)
+	   	}
+
+	   	if err = rows.Err(); err != nil {
+	   		log.Fatal("Rows error: ", err.Error())
+	   	}
+
+	   // Convert to JSON
+	   jsonData, err := json.Marshal(ccRes)
+
+	   	if err != nil {
+	   		log.Fatal("Error converting to JSON:", err.Error())
+	   	}
+
+	   // Print the JSON as a string
+	   fmt.Println(string(jsonData))
+	*/
+}
+
+func main() {
+	var db *sql.DB
+
+	db = db_create()
+	defer db.Close()
+
+	log.Println("Importing JSON files...")
+
+	const importStr string = `
+	CREATE OR REPLACE TABLE cc AS
+	(SELECT * FROM read_json_auto('**/*.JSON', ignore_errors=true))`
+
+	db_run_stmt_transactional(db, importStr)
+
+	log.Print("Running queries...")
+	cc_get_setter_count(db)
+}
